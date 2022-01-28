ec4c81506df166747c99ce5dae852df0f09263dc
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-01-28 20:22:15 +0100

    v1 hamburger react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { useOnClickOutside } from './hooks';
+import { GlobalStyles } from './components/styles/Global';
+import { theme } from './components/styles/theme';
+import Burger from './components/Burger'
+import Menu from './components/Menu'
+import React, { useState, useRef } from 'react';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const node = useRef(); 
+useOnClickOutside(node, () => setOpen(false));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <div>
+          <h1>Hello. This is a burger menu </h1>
+          <img src="https://media.giphy.com/media/xTiTnwj1LUAw0RAfiU/giphy.gif" alt="animated burger" />
+        </div>
+        <div ref={node}>
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
